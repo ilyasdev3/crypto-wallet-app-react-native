a2b917e033a7a9a1f45e5c88307d2b6f503c6d3d
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-08 09:40:16 +0500

    Add root types for native wind

---
+++ roots.types.ts
@@ -0,0 +1,11 @@
+export type RootStackParamList = {
+  cointDetails: undefined;
+  Login: undefined;
+  Register: undefined;
+  Profile: undefined;
+  Wallet: undefined;
+  Chat: undefined;
+  Explore: undefined;
+  EditProfile: undefined;
+  NewsFeed: undefined;
+};
