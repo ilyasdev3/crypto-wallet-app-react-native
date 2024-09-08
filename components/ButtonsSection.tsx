e1dc9b514e76a1098a03dde3dbc08e5c1496a4ea
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-08 09:43:09 +0500

    Add buttons sections

---
+++ components/ButtonsSection.tsx
@@ -0,0 +1,26 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+const ButtonsSection = () => {
+  return (
+    <View className="flex-row justify-between mt-6 w-full px-4">
+      <TouchableOpacity className="w-12 h-12 rounded-full bg-green-100 flex items-center justify-center">
+        <Text className="text-green-600">Buy</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity className="w-12 h-12 rounded-full bg-blue-100 flex items-center justify-center">
+        <Text className="text-blue-600">Send</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity className="w-12 h-12 rounded-full bg-yellow-100 flex items-center justify-center">
+        <Text className="text-yellow-600">Swap</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity className="w-12 h-12 rounded-full bg-red-100 flex items-center justify-center">
+        <Text className="text-red-600">More</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ButtonsSection;
