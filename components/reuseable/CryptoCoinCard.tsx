6a5df9807ffa36b321c024d4e2fb72944d07e52e
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-08 10:17:10 +0500

    Add navigation to coin details page

--- components/reuseable/CryptoCoinCard.tsx
+++ components/reuseable/CryptoCoinCard.tsx
@@ -1,25 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const CryptoCoinCard = ({ item }: any) => {
+  const navigation: any = useNavigation();
+
+  const handlePress = () => {
+    // Send the coin name as a route parameter in lowercase
+    navigation.navigate("CoinDetails", { coin: item.name.toLowerCase() });
+  };
+
   return (
-    <View className="flex-row items-center justify-between bg-[#F5F5F5] p-4 m-2 rounded-lg">
-      <View className="flex-row items-center">
-        <View
-          className={`${item.color} w-10 h-10 flex items-center justify-center rounded-full`}
-        >
-          <Text className="text-2xl">{item.icon}</Text>
+    <TouchableOpacity onPress={() => handlePress()}>
+      <View className="flex-row items-center justify-between bg-[#F5F5F5] p-4 m-2 rounded-lg">
+        <View className="flex-row items-center">
+          <View
+            className={`${item.color} w-10 h-10 flex items-center justify-center rounded-full`}
+          >
+            <Text className="text-2xl">{item.icon}</Text>
+          </View>
+          <View className="ml-4">
+            <Text className="text-lg font-semibold">{item.name}</Text>
+            <Text className="text-sm text-gray-500">Symbol: {item.symbol}</Text>
+          </View>
         </View>
-        <View className="ml-4">
-          <Text className="text-lg font-semibold">{item.name}</Text>
-          <Text className="text-sm text-gray-500">Symbol: {item.symbol}</Text>
+        <View className="flex items-end">
+          <Text className="text-lg font-semibold">{item.amount}</Text>
+          <Text className="text-sm text-gray-500">{item.usdValue}</Text>
         </View>
       </View>
-      <View className="flex items-end">
-        <Text className="text-lg font-semibold">{item.amount}</Text>
-        <Text className="text-sm text-gray-500">{item.usdValue}</Text>
-      </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
