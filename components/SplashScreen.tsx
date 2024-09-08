5c5994103cb3c58d8ebd1d96b2f7eea0347be60e
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-08 09:42:40 +0500

    Add splash screen

---
+++ components/SplashScreen.tsx
@@ -0,0 +1,28 @@
+// app/components/SplashScreen.tsx
+import React from "react";
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#0000ff" />
+      <Text style={styles.text}>Loading...</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    marginTop: 20,
+    fontSize: 18,
+    color: "#333",
+  },
+});
+
+export default SplashScreen;
