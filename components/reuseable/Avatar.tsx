bcde526dab35faad3701c4d76680820de37d5d81
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-14 22:10:12 +0500

    Update avatar component

--- components/reuseable/Avatar.tsx
+++ components/reuseable/Avatar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Avatar = ({ imageSource }: { imageSource: any }) => {
   return (
-    <View className="w-[40px] h-[40px] flex items-center justify-center rounded-full overflow-hidden bg-gray-300 p-[8]">
+    <View className="w-[40px] h-[40px] flex items-center justify-center rounded-full overflow-hidden bg-gray-300 p-[5]">
       <Image
         source={imageSource}
         className="w-full h-full rounded-full"
