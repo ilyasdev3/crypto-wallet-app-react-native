1401a2298ac8e4531071990390edb5f223a350e2
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-16 08:55:18 +0500

    Add user quries

--- lib/graphql/user/user.queries.ts
+++ lib/graphql/user/user.queries.ts
@@ -10,6 +10,27 @@ export const GET_CURRENT_USER = gql`
       lastName
       email
       bio
+      createdAt
+      updatedAt
+      following
+      followers
+    }
+  }
+`;
+export const GET_USER_BY_ID = gql`
+  query GetUser($getUserId: ID!) {
+    getUser(id: $getUserId) {
+      id
+      username
+      profileImage
+      firstName
+      lastName
+      email
+      bio
+      createdAt
+      updatedAt
+      following
+      followers
     }
   }
 `;
